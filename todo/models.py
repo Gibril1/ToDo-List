5fc77e247653adad81e3432c68cb3e88cc92a747
Gibril1 <gibrilissakaiddrisu@gmail.com>
2022-10-25 11:13:49 -0700

    Todo Completed

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    content = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now = True)
+    updated_at = models.DateField(auto_now_add = True)
+
+
+    def __str__(self) -> str:
+        return self.content
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from todo.forms import TodoForm
+from .models import Todo
 
 # Create your views here.
+def list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos':todos
+    }
+    return render(request,'todo/home.html', context)
+
+def update(request, pk):
+    todo = Todo.objects.get(id=pk)
+    form = TodoForm(instance=todo)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+        return redirect('todo-home')
+    context = {'form':form}
+    return render(request, 'todo/todo_form.html', context)
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo-home')
+    return render(request, 'todo/delete.html', {'todo':todo})
